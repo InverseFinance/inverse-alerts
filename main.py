b419fb849229af3eb064b063c6e50b04e6d04b33
Naoufel <80992298+naoufel-eth@users.noreply.github.com>
2022-05-18 21:05:28 +0000

    TODO : Add state variable

--- main.py
+++ main.py
@@ -15,28 +15,34 @@ web3 = Web3(Web3.HTTPProvider(ENDPOINT))
 
 alerts = ['dola3crv', 'lending', 'governance']
 
+#TODO: include state variables reading at handling
+
 functions_lending = ['Mint', 'Redeem', 'Borrow', 'RepayBorrow']
 functions_governance = ['ProposalCreated', 'ProposalCanceled', 'ProposalQueued', 'ProposalExecuted']
 functions_dola3crv = ['AddLiquidity', 'RemoveLiquidity', 'RemoveLiquidityOne']
+
+state_lending = ['name','totalBorrows','totalReserves','totalSupply','getCash']
+state_governance = ['proposalCount']
+state_dola3crv = ['name','symbol','totalSupply']
 
 webhook_dola3crv = os.getenv('webhook_dola3crv')
 webhook_governance = os.getenv('webhook_governance')
 webhook_lending = os.getenv('webhook_lending')
 
 
-class MyThread(Thread):
-    def __init__(self, argument0, argument1, argument2, **kwargs):
-        super(MyThread, self).__init__(**kwargs)
-        self.argument0 = argument0
-        self.argument1 = argument1
-        self.argument2 = argument2
+class Listener(Thread):
+    def __init__(self, alert, contract, function, **kwargs):
+        super(Listener, self).__init__(**kwargs)
+        self.alert = alert
+        self.contract = contract
+        self.function = function
         self.event_filter = []
 
     def run(self):
-        exec(f"self.event_filter = self.argument1.events.{self.argument2}.createFilter(fromBlock='latest')")
+        exec(f"self.event_filter = self.contract.events.{self.function}.createFilter(fromBlock='latest')")
         while True:
             for handler in self.event_filter.get_new_entries():
-                handle_event(handler, self.argument0)
+                handle_event(handler, self.alert, self.contract)
 
 
 # define function to handle events and print to the console
@@ -49,11 +55,11 @@ def handle_event(event, alert):
                  "\n" + "Block Number : " + str(tx['blockNumber']) +
                  "\n" + "Transaction Hash : " + tx['transactionHash'] +
                  "\n" + "Etherscan : https://etherscan.io/tx/" + tx['transactionHash'] +
-                 "\n" + "Full tx" + str(tx) +
-                 "\n" + "Tag Test <@578956365205209098>")
+                 "\n" + "Full tx" + str(tx)+
+                 "\n" +  Web3)
+                 #"\n" + "Tag Test <@578956365205209098>")
 
-
-def main():
+if __name__ == "__main__":
     nalert = 0
     for alert in alerts:
         # Get contracts filter ABIs and contract by alert name
@@ -68,17 +74,15 @@ def main():
             contract_abi = json.loads(contracts.iloc[i]['ABI'])
             filters["id"].append(web3.eth.contract(address=contract_name, abi=contract_abi))
 
+        functions = ""
         exec(f'functions = functions_{alert}')
         exec(f'webhook = Webhook.from_url(webhook_{alert}, adapter=RequestsWebhookAdapter())')
+
         for i in filters["id"]:
             contract = i
             for j in functions:
                 function = j
-                MyThread(alert, contract, function).start()
+                Listener(alert, contract, function).start()
                 nalert += 1
-                print('Started listening at function ' + function + ' on contract ' + contract.address)
-                print('Alerts running : ' + str(nalert))
-
-
-if __name__ == "__main__":
-    main()
+                print('Alert '+ alert +' started listening at function ' + function + ' on contract ' + contract.address +' (n.'+ str(nalert) +')')
+    print('Alerts running : ' + str(nalert))
