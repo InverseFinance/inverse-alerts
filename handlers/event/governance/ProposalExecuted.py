464a38cee9d1a46920d77efa203d1976f3a3673f
naouflex <naoufel.eth@gmail.com>
2023-01-17 21:46:21 +0100

    update tag

--- handlers/event/governance/ProposalExecuted.py
+++ handlers/event/governance/ProposalExecuted.py
@@ -22,7 +22,7 @@ class handler():
         blockNumber = self.tx["blockNumber"]
         transactionHash = self.tx["transactionHash"]
 
-        self.content = "<@&899302193608409178>"
+        self.content = "<@&1059935747362279475>"
         self.webhook = os.getenv('WEBHOOK_GOVERNANCE')
 
         self.title = "Governor Mills : New " + re.sub(r"(\w)([A-Z])", r"\1 \2", str(self.tx["event"]))
